83e5e858b74ef293c822377e803d7fec48c4f919
jyan <fssyan@gmail.com>
2025-07-06 14:02:45 -0400

    traveling salesman

--- min_cost_to_visit_every_node.py
+++ min_cost_to_visit_every_node.py
@@ -1,27 +1,31 @@
 import heapq
+from collections import deque
+
 
 def min_cost_to_visit_every_node(graph: list[list[int]]) -> int:
     n = len(graph)
-    min_cost = [float('inf') for _ in range(2 ** n)]
-    min_cost[1] = 0
-    heap = [(1, 0, 0)]
-    while heap:
-        bitmask, node, cost_to_visit = heapq.heappop(heap)
-        if cost_to_visit > min_cost[bitmask]:
+    dp = {(1, 0): 0}
+    queue = deque([(1, 0, 0)])
+
+    while queue:
+        bitmask, node, cost = queue.popleft()
+        if cost > dp[(bitmask, node)]:
             continue
 
-        for neighbor in range(n):
-            if graph[node][neighbor] == 0:
-                continue # edge does not exist
+        for adj in range(n):
+            if graph[node][adj] != 0:
+                cost_to_adj = cost + graph[node][adj]
+                bitmask_adj = bitmask | (1 << adj)
+                if (bitmask_adj, adj) not in dp or dp[(bitmask_adj, adj)] > cost_to_adj:
+                    dp[(bitmask_adj, adj)] = cost_to_adj
+                    queue.append((bitmask_adj, adj, cost_to_adj))
 
-            bitmask_neighbor = bitmask | (1 << neighbor)
-            if bitmask_neighbor != bitmask: # do not revisit previous node
-                cost_neighbor = cost_to_visit + graph[node][neighbor]
-                if cost_neighbor < min_cost[bitmask_neighbor]:
-                    min_cost[bitmask_neighbor] = cost_neighbor
-                    heapq.heappush(heap, (bitmask_neighbor, neighbor, cost_neighbor))
-
-    return -1 if min_cost[2 ** n - 1] == float('inf') else min_cost[2 ** n - 1]
+    min_cost = float('inf')
+    all_visited = (1 << n) - 1
+    for node in range(n):
+        if (all_visited, node) in dp:
+            min_cost = min(min_cost, dp[(all_visited, node)])
+    return -1 if min_cost == float('inf') else min_cost
 
 if __name__ == '__main__':
     assert min_cost_to_visit_every_node([[0, 2], [2, 0]]) == 2
