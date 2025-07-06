d8e1edb3188e86af8d4437012c9d9d400d9755d1
jyan <fssyan@gmail.com>
2025-07-06 14:53:43 -0400

    traveling salesman

--- min_cost_to_visit_every_node.py
+++ min_cost_to_visit_every_node.py
@@ -11,7 +11,7 @@ def min_cost_to_visit_every_node(graph: list[list[int]]) -> int:
             continue
 
         for adj in range(n):
-            if graph[node][adj] != 0:
+            if graph[node][adj] != 0 and bitmask & (1 << adj) == 0:
                 cost_to_adj = cost + graph[node][adj]
                 bitmask_adj = bitmask | (1 << adj)
                 if (bitmask_adj, adj) not in dp or dp[(bitmask_adj, adj)] > cost_to_adj:
@@ -42,3 +42,16 @@ if __name__ == '__main__':
         [2, 1, 0, 2, 2],
         [2, 2, 1, 0, 2],
         [2, 2, 2, 2, 0]]) == 4
+    graph = [
+        [0, 541, 246, 858, 384, 651, 494, 777, 711, 616],
+        [863, 0, 497, 75, 0, 0, 531, 172, 667, 353],
+        [254, 999, 0, 397, 54, 356, 77, 368, 349, 851],
+        [927, 96, 291, 0, 272, 579, 0, 95, 361, 506],
+        [266, 277, 264, 17, 0, 455, 472, 396, 211, 239],
+        [797, 505, 405, 121, 104, 0, 566, 38, 369, 160],
+        [379, 829, 719, 0, 477, 210, 0, 363, 481, 987],
+        [270, 0, 779, 165, 260, 93, 446, 0, 850, 527],
+        [995, 0, 99, 149, 696, 155, 882, 335, 0, 646],
+        [502, 79, 766, 0, 7, 367, 832, 788, 605, 0]
+    ]
+    assert min_cost_to_visit_every_node(graph) == 1328
