68cd017adf000fe08671cb0c73fb00c3cd1cd552
jyan <fssyan@gmail.com>
2025-08-01 17:06:03 -0400

    max uncrossed lines

---
+++ max_uncrossed_lines.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        return dp[m][n]
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maxUncrossedLines([1,4,2], [1,2,4]))
+    assert s.maxUncrossedLines([1,4,2], [1,2,4]) == 2
+    assert s.maxUncrossedLines([2,5,1,2,5], [10,5,2,1,5,2]) == 3
+    assert s.maxUncrossedLines([1,3,7,1,7,5], [1,9,2,5,1]) == 2
